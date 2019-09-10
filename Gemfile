3bdb9c1f0ca5a5c65c9184beb7be395af502b12e
Gabriel Gonzalez <gabokappa@gmail.com>
2019-09-10 15:01:08 +0100

    Ch9 completed test database created spec and feature tests changed to create data entries and spec helper changed to truncate the test database

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'sinatra'
 group :test do
   gem 'capybara'
   gem 'rspec'
-  gem 'rubocop', '0.56.0'
+  gem 'rubocop', '0.56.0', require: false
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
 end
--- spec/features/bookmark_manager_spec.rb
+++ spec/features/bookmark_manager_spec.rb
@@ -1,14 +1,21 @@
 require './spec/spec_helper'
+require 'pg'
 
 feature 'testing page' do
   scenario 'page should dislpay hello world' do
     visit '/'
     expect(page).to have_content 'Bookmark Manager'
   end
-end 
+end
 
 feature 'viewing bookmarks' do
   scenario 'displays bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+
     visit '/bookmarks'
     expect(page).to have_content 'http://www.makersacademy.com'
     expect(page).to have_content 'http://www.destroyallsoftware.com'
